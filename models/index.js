0cc129d8bf354aed771bc0681958167a39f78500
Soprano <antfar67@gmail.com>
2022-12-31 12:53:21 -0800

    Added category routes

--- models/index.js
+++ models/index.js
@@ -19,7 +19,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: "product_tags"
+  as: "products_tags"
 })
 
 // Tags belongToMany Products (through ProductTag)
@@ -28,7 +28,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
-  as: "tag_products"
+  as: "tags_products"
 })
 
 module.exports = {
