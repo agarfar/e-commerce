14aeef8a985c5497b805a46ce457a72f4b32d614
Soprano <antfar67@gmail.com>
2023-01-03 18:18:37 -0800

    removing unnecessary comments

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }, { model: ProductTag }],
+      include: [{ model: Product, through: ProductTag, as: 'products' }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: ProductTag }],
+      include: [{ model: Product, through: ProductTag, as: 'products' }],
     });
     if (!tagData) {
       res.status(404).json({ message: 'No tag with this id!' });
@@ -37,7 +37,7 @@ router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tagData = await Tag.create({
-      id: req.body.id,
+      // id: req.body.id,
       tag_name: req.body.tag_name,
     });
     res.status(200).json(tagData);
